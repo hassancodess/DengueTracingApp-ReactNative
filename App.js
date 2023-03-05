aef4e6b29f9ba4b6c3d401ae83f9571d209ad7d5
hassancodess <dev.hassancodes@gmail.com>
2023-03-05 13:46:27 +0500

    Splash finally added xD

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MainStackNavigator from './src/navigation/MainStackNavigator';
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return <MainStackNavigator />;
 };
 
